932085bba34164ebf4cf3796c175bec1a1527deb
CarnageOG <egetashviligiorgi9@gmail.com>
2025-02-14 15:23:02 +0400

    made secend type of sentence

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -14,6 +14,9 @@ let favoriteActivity = "a perfect day for Gaming";
 let sentence = name + " " + dayOfWeek +  " " + favoriteActivity;
 console.log(sentence);
 
+let quote = "He said that \"Hi, my name is Giorgi. Today is February fourteenth—a perfect day for gaming.\"";
+console.log(quote);
+
 
 
 ////////////////////////////////
