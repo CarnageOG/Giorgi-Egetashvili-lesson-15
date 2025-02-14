2af905cd73d8f9241461094436219a064fb8fae9
CarnageOG <egetashviligiorgi9@gmail.com>
2025-02-14 15:22:18 +0400

    made first type of sentence

---
+++ JavaScript/app.js
@@ -0,0 +1,19 @@
+const userName = "Nodari";
+const userHeight = "184";
+const isUserMan = true;
+let userWeight;
+let userFamily = null;
+
+
+
+//////////////////////////////////
+const name = "Hi, my name is Giorgi";
+let dayOfWeek = "Today is february fourteenth";
+let favoriteActivity = "a perfect day for Gaming";
+
+let sentence = name + " " + dayOfWeek +  " " + favoriteActivity;
+console.log(sentence);
+
+
+
+////////////////////////////////
